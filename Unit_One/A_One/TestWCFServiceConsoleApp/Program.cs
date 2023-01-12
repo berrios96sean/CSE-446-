20e760751597bd5a313b1be092d7b557a8fdcbb2
Sean Berrios <berrios96sean@gmail.com>
2023-01-11 23:26:55 -0700

    Implemented Program.cs code to test service

--- Unit_One/A_One/TestWCFServiceConsoleApp/Program.cs
+++ Unit_One/A_One/TestWCFServiceConsoleApp/Program.cs
@@ -10,6 +10,17 @@ namespace TestWCFServiceConsoleApp
     {
         static void Main(string[] args)
         {
+            ServiceReference1.Service1Client myPxy = new ServiceReference1.Service1Client();
+            string str = myPxy.Hello();
+            double pi = myPxy.PiValue();
+            Int32 test1 = 27;
+            Int32 test2 = -132;
+            Int32 result1 = myPxy.absValue(test1);
+            Int32 result2 = myPxy.absValue(test2);
+            Console.WriteLine("Hello returns {0}", str);
+            Console.WriteLine("PI value = {0}", pi );
+            Console.WriteLine("Abs Values 0f {0} is {1} and of {2} is {3}", test1,result1,test2,result2);
+            myPxy.Close(); 
         }
     }
 }
