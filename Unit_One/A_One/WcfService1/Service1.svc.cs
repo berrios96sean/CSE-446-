0d9fdb5eeb5bf474744038653fc3e843122924ad
Sean Berrios <berrios96sean@gmail.com>
2023-01-11 23:08:28 -0700

    implemented service

--- Unit_One/A_One/WcfService1/Service1.svc.cs
+++ Unit_One/A_One/WcfService1/Service1.svc.cs
@@ -12,6 +12,24 @@ namespace WcfService1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+        public string Hello()
+        {
+            return "Hello World";
+        }
+
+        public double PiValue()
+        {
+            double pi = System.Math.PI;
+            return pi; 
+        }
+
+        public int absValue(int x)
+        {
+            if (x >= 0) return x;
+            else return (-x); 
+        }
+
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
