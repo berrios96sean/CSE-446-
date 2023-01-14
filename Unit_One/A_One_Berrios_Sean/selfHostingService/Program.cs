21c28a4f9391752aba23bda37e0ea80d5288098a
Sean Berrios <berrios96sean@gmail.com>
2023-01-14 11:44:09 -0700

    created main to create endpoint for the service

--- Unit_One/A_One_Berrios_Sean/selfHostingService/Program.cs
+++ Unit_One/A_One_Berrios_Sean/selfHostingService/Program.cs
@@ -27,5 +27,32 @@ namespace selfHostingService
             if (x >= 0) return (x);
             else return (-x); 
         }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Uri baseAddress = new Uri("http://localhost:8000/Service");
+                ServiceHost selfHost = new ServiceHost(typeof(myService),baseAddress);
+
+                try
+                {
+                    selfHost.AddServiceEndpoint(typeof(myInterface),new WSHttpBinding(),"myService");
+                    System.ServiceModel.Description.ServiceMetadataBehavior smb = new System.ServiceModel.Description.ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+                    selfHost.Description.Behaviors.Add(smb);
+                    selfHost.Open();
+                    Console.WriteLine("My Service is ready to take requests. Please Create a client to call my double PiValue() service or int absValue(int) service");
+                    Console.WriteLine("If you want to quit this service simply press <Enter>. \n ");
+                    Console.ReadLine();
+                    selfHost.Close(); 
+                }
+                catch(CommunicationException ce)
+                {
+                    Console.WriteLine("An Exception occurred: {0}", ce.Message);
+                    selfHost.Abort(); 
+                }
+            }
+        }
     }
 }
