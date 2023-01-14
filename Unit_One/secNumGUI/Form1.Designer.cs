31747600b6aadcbd9ed87e127376384840ad94fd
Sean Berrios <berrios96sean@gmail.com>
2023-01-13 23:47:40 -0700

    completed windows form with service

--- Unit_One/secNumGUI/Form1.Designer.cs
+++ Unit_One/secNumGUI/Form1.Designer.cs
@@ -38,6 +38,8 @@ namespace secNumGUI
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -127,11 +129,31 @@ namespace secNumGUI
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(322, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Attempts";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(389, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "- The number is ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
@@ -159,6 +181,8 @@ namespace secNumGUI
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Unit_One/secNumGUI/Form1.cs
+++ Unit_One/secNumGUI/Form1.cs
@@ -12,6 +12,8 @@ namespace secNumGUI
 {
     public partial class Form1 : Form
     {
+        public int number;
+        public int attempts; 
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +21,24 @@ namespace secNumGUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            ServiceReference1.Service1Client mySvc = new ServiceReference1.Service1Client();
+            this.attempts++;
+            label5.Text = attempts.ToString();
+            int userNum = Int32.Parse(textBox3.Text);
+            string num = mySvc.checkNumber(userNum,this.number);
+            label6.Text = num; 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            ServiceReference1.Service1Client mySvc = new ServiceReference1.Service1Client();
+            this.attempts = 0; 
+            int lower = Int32.Parse(textBox1.Text);
+            int upper = Int32.Parse(textBox2.Text);
+            this.number = mySvc.SecretNumber(lower,upper);
+            label5.Text = attempts.ToString(); 
         }
+
+
     }
 }
