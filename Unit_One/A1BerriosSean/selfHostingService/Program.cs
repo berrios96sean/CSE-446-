0756fff346cce5c48f2e6a3b2186d4944b716fe3
Sean Berrios <berrios96sean@gmail.com>
2023-01-15 15:05:46 -0700

    finished selfhosting service

---
+++ Unit_One/A1BerriosSean/selfHostingService/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace selfHostingService
+{
+    [ServiceContract]
+    public interface myInterface
+    {
+        [OperationContract]
+        int SecretNumber(int lower, int upper);
+
+        [OperationContract]
+        string checkNumber(int userNum, int SecretNum);
+    }
+
+    public class myService : myInterface
+    {
+        public int SecretNumber(int lower, int upper)
+        {
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lower, upper);
+            return sNumber;
+        }
+        public string checkNumber(int userNum, int SecretNum)
+        {
+            if (userNum == SecretNum)
+                return "correct";
+            else
+            if (userNum > SecretNum)
+                return "too big";
+            else return "too small";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/Service");
+            ServiceHost selfHost = new ServiceHost(typeof(myService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(myInterface), new WSHttpBinding(), "myService");
+                System.ServiceModel.Description.ServiceMetadataBehavior smb = new System.ServiceModel.Description.ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+                selfHost.Open();
+                Console.WriteLine("My Service is ready to take requests. Please create a client to call my tnt SecretNumber(int,int) service or string CheckNumber(int,int) service. ");
+                Console.WriteLine("If you want to quit this service, press <ENTER>. \n");
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
---
+++ Unit_One/A1BerriosSean/selfHostingService/selfHostingClient/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selfHostingClient
+{
+    class Program
+    {
+        static myInterfaceClient myPxy = new myInterfaceClient();
+        static int attempts = 0;
+        static bool gameOver = false;
+        static int gamesPlayed = 0;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (gameOver == false)
+                {
+                    playGame();
+                }
+                else
+                {
+                    Console.WriteLine("If you would like to play the game again press <y>");
+                    if (Console.ReadLine() == "y")
+                    {
+                        playGame();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+
+            }
+
+        }
+
+        public static int makeGuess()
+        {
+            Console.WriteLine("Please enter a guess");
+            int guess = Int32.Parse(Console.ReadLine());
+            return guess;
+        }
+
+
+        public static string checkGuess(int guess, int num)
+        {
+            string result = myPxy.checkNumber(guess, num);
+            return result;
+        }
+
+        public static void playGame()
+        {
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("Welcome To Number Guessing Game");
+            Console.WriteLine("Total Games Played = {0}", gamesPlayed);
+            Console.WriteLine("Enter Lower Limit: ");
+            int lower = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Upper Limit: ");
+            int upper = Int32.Parse(Console.ReadLine());
+            int num = myPxy.SecretNumber(lower, upper);
+            string res = "";
+            while (res != "correct")
+            {
+                Console.WriteLine("*********************************************");
+                attempts++;
+                Console.WriteLine("Attempt: {0}", attempts);
+                int guess = makeGuess();
+                res = checkGuess(guess, num);
+                Console.WriteLine(res);
+                Console.WriteLine("*********************************************");
+            }
+
+            gamesPlayed++;
+            gameOver = true;
+
+            attempts = 0;
+
+        }
+    }
+}
