1c06e8a1dfe9271e7633b4359aa112d26a7d43e5
Sean Berrios <berrios96sean@gmail.com>
2023-01-17 12:44:53 -0700

    Implemented service code

--- Unit_Two/A2Practice/RestServicePractice/Service1.svc.cs
+++ Unit_Two/A2Practice/RestServicePractice/Service1.svc.cs
@@ -12,22 +12,21 @@ namespace RestServicePractice
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public double PiValue()
         {
-            return string.Format("You entered: {0}", value);
+            double pi = System.Math.PI;
+            return (pi);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public int absValue(int x)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            if (x >= 0) return (x);
+            else return (-x); 
+        }
+
+        public int addition(int x, int y)
+        {
+            return (x + y); 
         }
     }
 }
