7adeb4623e19af39617752b798e0626bf88503d5
Sean Berrios <berrios96sean@gmail.com>
2023-01-17 12:38:28 -0700

    created interface code for project

--- Unit_Two/A2Practice/RestServicePractice/IService1.cs
+++ Unit_Two/A2Practice/RestServicePractice/IService1.cs
@@ -12,36 +12,19 @@ namespace RestServicePractice
     [ServiceContract]
     public interface IService1
     {
+        [OperationContract]
+        [WebGet]
+        double PiValue();
 
         [OperationContract]
-        string GetData(int value);
+        [WebGet]
+        int absValue(int x);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        [WebGet]
+        int addition(int x, int y); 
     }
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
 }
