b1246291c14e2a0cf25b828b727f8b82c80e6cf4
Sean Berrios <berrios96sean@gmail.com>
2023-01-18 23:01:02 -0700

    Implemented interface

--- Unit_Two/A2Practice/ImageVerifier/IService1.cs
+++ Unit_Two/A2Practice/ImageVerifier/IService1.cs
@@ -12,36 +12,12 @@ namespace ImageVerifier
     [ServiceContract]
     public interface IService1
     {
+        [WebGet(UriTemplate = "/GetRandomString", RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare)]
+        string GetRandomString0();
 
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        [WebGet(UriTemplate = "/GetRandomString/{Length}", RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare)]
+        string GetRandomString(string Length);
     }
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
 }
