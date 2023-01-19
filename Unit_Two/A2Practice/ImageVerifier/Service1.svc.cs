5ab12f1a8ba2b2a5e1b380a7396006de63622779
Sean Berrios <berrios96sean@gmail.com>
2023-01-18 23:39:49 -0700

    finished service implementation

--- Unit_Two/A2Practice/ImageVerifier/Service1.svc.cs
+++ Unit_Two/A2Practice/ImageVerifier/Service1.svc.cs
@@ -31,7 +31,39 @@ namespace ImageVerifier
 
         public Stream GetImage(string myString)
         {
-
+            WebOperationContext.Current.OutgoingResponse.ContentType = "image/jpeg";
+            int mapwidth = (int)(myString.Length * 25);
+            Bitmap bMap = new Bitmap(mapwidth, 40);
+            Graphics graph = Graphics.FromImage(bMap);
+            graph.Clear(Color.Azure);
+            graph.DrawRectangle(new Pen(Color.LightBlue, 0), 0, 0, bMap.Width - 1, bMap.Height - 1);
+            Random rand = new Random();
+            Pen badPen = new Pen(Color.LightGreen, 0); 
+            for (int i = 0; i < 100; i++)
+            {
+                int x = rand.Next(1, bMap.Width - 1);
+                int y = rand.Next(1, bMap.Height - 1);
+                graph.DrawRectangle(badPen, x, y, 4, 3);
+                graph.DrawEllipse(badPen, x, y, 2, 3); 
+            }
+            char[] charString = myString.ToCharArray();
+            Font font = new Font("Boopee", 18, FontStyle.Bold);
+            Color[] clr = { Color.Black, Color.Red, Color.DarkViolet, Color.Green, Color.DarkOrange, Color.Brown, Color.Goldenrod, Color.Plum }; 
+            for (int i = 0; i < myString.Length; i++)
+            {
+                int d = rand.Next(20, 25);
+                int p = rand.Next(1, 15);
+                int c = rand.Next(0, 7);
+                string str1 = Convert.ToString(charString[i]);
+                Brush b = new System.Drawing.SolidBrush(clr[c]);
+                graph.DrawString(str1, font, b, 1 + i * d, p);
+            }
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            bMap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            ms.Position = 0;
+            graph.Dispose();
+            bMap.Dispose();
+            return ms; 
         }
         
     }
