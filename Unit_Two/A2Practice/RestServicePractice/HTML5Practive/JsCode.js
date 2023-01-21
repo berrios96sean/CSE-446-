52b2969f6cfe964038fdb56eb44065c6ec6e93aa
Sean Berrios <berrios96sean@gmail.com>
2023-01-20 23:52:19 -0700

    rebuild

--- Unit_Two/A2Practice/RestServicePractice/HTML5Practive/JsCode.js
+++ Unit_Two/A2Practice/RestServicePractice/HTML5Practive/JsCode.js
@@ -5,3 +5,14 @@
 function multiply2Nos() {
     document.sum.z.value = parseInt(document.sum.x.value) * parseInt(document.sum.y.value)
 }
+
+function ServiceCall() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange == function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200)
+            document.sum.s.value = xhttp.responseText;
+    }
+    xhttp.open("GET", "http://localhost:60331/Service1.svc/add2?x=" + document.sum.x.value + "&y=" + document.sum.y.value, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
