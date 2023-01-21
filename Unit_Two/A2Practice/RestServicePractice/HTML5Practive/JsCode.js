dd74b65d5a350673496d8dcfc805304f994e3ed3
Sean Berrios <berrios96sean@gmail.com>
2023-01-20 23:41:06 -0700

    testing

---
+++ Unit_Two/A2Practice/RestServicePractice/HTML5Practive/JsCode.js
@@ -0,0 +1,4 @@
+﻿function add2Nos() {
+    document.getElementsByName.x
+    //document.getElementById.z.value = parseInt(document.getElementById.x.value) + parseInt(document.getElementById.y.value)
+}
