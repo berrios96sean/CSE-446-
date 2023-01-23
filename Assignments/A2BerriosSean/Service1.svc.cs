96160080ace0507d648496175d9b221af67b15e1
Sean Berrios <berrios96sean@gmail.com>
2023-01-22 23:47:10 -0700

    first build

--- Assignments/A2BerriosSean/Service1.svc.cs
+++ Assignments/A2BerriosSean/Service1.svc.cs
@@ -11,6 +11,34 @@ namespace A2BerriosSean
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
+    { 
+        public int SecretNumber(int lower, int upper)
+        {
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lower, upper);
+            return sNumber; 
+        }
+
+        public string checkNumber(int userNum,int secretNum)
+        {
+            if (userNum == secretNum)
+            {
+                return "correct";
+            }
+            else
+            {
+                if (userNum > secretNum)
+                {
+                    return "too big";
+                }
+                else
+                {
+                    return "too small";
+                }
+            }
+        }
 
     }
 }
