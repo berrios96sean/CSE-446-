4f9b9f0f4ed7f82d4ef2b3c598f2aa384b4ab564
Sean Berrios <berrios96sean@gmail.com>
2023-01-22 23:54:40 -0700

    initial creation for html5 web app

---
+++ Assignments/A2BerriosSean/webAppHTML5Client/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace webAppHTML5Client.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
