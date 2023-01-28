0aba260ddb2ddf22cb6a24019f9abf5b0ff0665b
Sean Berrios <berrios96sean@gmail.com>
2023-01-27 18:47:44 -0700

    testing

--- Assignments/A2BerriosSean/webAppHTML5Client/JsCode.js
+++ Assignments/A2BerriosSean/webAppHTML5Client/JsCode.js
@@ -1,7 +1,18 @@
 ﻿var secNum; 
 function genSecNum() {
-    secNum = document.game.lowerLimit.value;
-    document.game.upperLimit.value = secNum; 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status = 200 ) {
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(xhttp.responseText, "text/html");
+            var result = xmlDoc.getElementsByTagName("int")[0].textContent;
+            secNum = result; 
+        }
+    }
+
+    xhttp.open("GET", "http://localhost:60331/Service1.svc/add2?x=" + document.sum.x.value + "&y=" + document.sum.y.value, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
 }
 
 function playGam() {
