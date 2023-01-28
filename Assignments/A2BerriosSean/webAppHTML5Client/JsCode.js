064427aad67db78f314ab136d71652ee376d709b
Sean Berrios <berrios96sean@gmail.com>
2023-01-27 23:42:15 -0700

    testing

--- Assignments/A2BerriosSean/webAppHTML5Client/JsCode.js
+++ Assignments/A2BerriosSean/webAppHTML5Client/JsCode.js
@@ -1,5 +1,5 @@
-﻿function genSecretNum() {
-    console.log("test");
+﻿var secretNum; 
+function genSecretNum() {
     var lower = document.game.lowerLimit.value;
     var upper = document.game.upperLimit.value; 
     var xhttp = new XMLHttpRequest();
@@ -8,14 +8,27 @@
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
             var result = xmlDoc.getElementsByTagName("int")[0].textContent;
-            document.game.userGuess.value = result; 
+            secretNum = result;  
         }
     };
-    xhttp.open("GET", "http://localhost:50719/Service1.svc/secretNum?lower=1&upper=10", true);
+    xhttp.open("GET", "http://localhost:50719/Service1.svc/secretNum?lower="+lower+"&upper="+upper, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 }
 
 function playGame() {
-
+    var guess = document.game.userGuess.value;
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
+            var result = xmlDoc.getElementsByTagName("int")[0].textContent;
+            document.game.result.value = result; 
+        }
+    };
+    xhttp.open("GET", "checkNum?userNum="+guess+"&secretNum="+secretNum, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
 }
