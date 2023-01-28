0b9aeaf026173b05333e18980f88876cd0ad31fb
Sean Berrios <berrios96sean@gmail.com>
2023-01-27 23:56:21 -0700

    finsihed implementation

--- Assignments/A2BerriosSean/webAppHTML5Client/JsCode.js
+++ Assignments/A2BerriosSean/webAppHTML5Client/JsCode.js
@@ -1,14 +1,17 @@
-﻿var secretNum; 
+﻿var secretNum;
+var attempts; 
 function genSecretNum() {
     var lower = document.game.lowerLimit.value;
-    var upper = document.game.upperLimit.value; 
+    var upper = document.game.upperLimit.value;
+    attempts = 0; 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
             var result = xmlDoc.getElementsByTagName("int")[0].textContent;
-            secretNum = result;  
+            secretNum = result;
+            document.getElementById("attempts").innerHTML = attempts;
         }
     };
     xhttp.open("GET", "http://localhost:50719/Service1.svc/secretNum?lower="+lower+"&upper="+upper, true);
@@ -16,19 +19,18 @@ function genSecretNum() {
     xhttp.send();
 }
 
-function playGame() {
+function play_Game() {
     var guess = document.game.userGuess.value;
-    
+    attempts++;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-            var parser = new DOMParser();
-            var xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
-            var result = xmlDoc.getElementsByTagName("int")[0].textContent;
-            document.game.result.value = result; 
+            var result = xhttp.responseText;
+            document.getElementById("result").innerHTML = result;
+            document.getElementById("attempts").innerHTML = attempts; 
         }
     };
-    xhttp.open("GET", "checkNum?userNum="+guess+"&secretNum="+secretNum, true);
+    xhttp.open("GET", "http://localhost:50719/Service1.svc/checkNum?userNum="+guess+"&secretNum="+secretNum, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 }
